9268bcb350e44e050bd464ae84b124e8aa439d40
VolkovDV <snake1234567890-1@yandex.ru>
2020-11-23 21:42:56 +0300

    Made section Girls

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ $('.slider-girls').owlCarousel({
     loop:true,
     margin:30,
     nav:true,
+    dots: false,
+    navText: [
+        '<span class="arrow-owl arrow-left"></span>',
+        '<span class="arrow-owl arrow-right"></span>'
+      ],
     responsive:{
         0:{
             items:1
@@ -13,4 +18,26 @@ $('.slider-girls').owlCarousel({
             items:3
         }
     }
-})
+});
+
+$('.slider-programs').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    navText: [
+        '<span class="arrow-owl arrow-left"></span>',
+        '<span class="arrow-owl arrow-right"></span>'
+      ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
